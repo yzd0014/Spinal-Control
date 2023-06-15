29e3161f11ecd8efffb8d9b3d6e8a00cf03d3b34
yzd0014 <1051714925@qq.com>
2023-06-14 23:51:37 -0700

    options to choose between two env added

--- train_pendulum.py
+++ train_pendulum.py
@@ -6,10 +6,16 @@ import pendulum_env
 import double_links_env
 import time
 
-control_type = double_links_env.Control_Type.BASELINE
+env_id = 0
 
 models_dir = f"models/{int(time.time())}/"
-logdir = f"logs/{int(time.time())}-{double_links_env.control_typle_dic[control_type]}/"
+
+control_type = pendulum_env.Control_Type.RI_AND_REFLEX
+logdir = f"logs/{int(time.time())}-{pendulum_env.control_typle_dic[control_type]}/"
+if env_id == 1:
+	control_type = double_links_env.Control_Type.BASELINE
+	logdir = f"logs/{int(time.time())}-{double_links_env.control_typle_dic[control_type]}/"
+
 
 if not os.path.exists(models_dir):
 	os.makedirs(models_dir)
@@ -17,11 +23,10 @@ if not os.path.exists(models_dir):
 if not os.path.exists(logdir):
 	os.makedirs(logdir)
 
-# env = pendulum_env.PendulumEnv(control_type=control_type)
+env = pendulum_env.PendulumEnv(control_type=control_type)
 # vec_env = make_vec_env(lambda: pendulum_env.PendulumEnv(control_type=control_type), n_envs=4)
-
-env = double_links_env.DoubleLinkEnv(control_type=control_type)
-
+if env_id == 1:
+	env = double_links_env.DoubleLinkEnv(control_type=control_type)
 
 model = PPO('MlpPolicy', env, device='cpu', n_steps=50000, batch_size=10000, n_epochs=100, verbose=1, tensorboard_log=logdir)
 # PPO_model_path="models/1683788483/11830000.zip"
