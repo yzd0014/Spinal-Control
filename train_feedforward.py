febbea8107bf89e644c44e536df1c35b22ca9886
Yitong Dai <1051714925@qq.com>
2023-10-04 11:15:14 -0700

    fixed all syntax error, still need to fix logic errors

--- train_feedforward.py
+++ train_feedforward.py
@@ -11,6 +11,7 @@ class FeedForwardNN(nn.Module):
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, output_size)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.fc1(x)
@@ -18,14 +19,14 @@ class FeedForwardNN(nn.Module):
         x = self.fc2(x)
         x = self.relu(x)
         x = self.fc3(x)
+        x = self.sigmoid(x)
         return x
 
 # create a feedforward neural network
 input_size = 6
-hidden_size = 64
+hidden_size = 8
 output_size = 4
 net = FeedForwardNN(input_size, hidden_size, output_size)
-clamped_net = torch.clamp(net, 0, 1)
 
 # create training data
 episdoe_length = 200
@@ -37,6 +38,7 @@ for i in np.arange(-max_training_angle, max_training_angle, angle_interval):
     for j in np.arange(-max_training_angle, max_training_angle, angle_interval):
         traning_samples.append(np.array([i, j]))
         num_of_targets += 1
+print(f"total number of training samples: {num_of_targets}")
 
 # traning configuration
 num_epochs = 100
@@ -50,45 +52,60 @@ xml_path = abspath
 model = mj.MjModel.from_xml_path(xml_path)  # MuJoCo model
 data = mj.MjData(model)  # MuJoCo data
 
+optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 for epoch in range(num_epochs):
+    mean_ep_loss = 0
     for batch_id in range(num_of_targets):
+        # reset at the beginning of each episode
         batch_size = episdoe_length
         mj.mj_resetData(model, data)
         mj.mj_forward(model, data)
         target_pos = np.array(traning_samples[batch_id])
+
+        # set optimizer
+        optimizer.zero_grad()
+
+        #track loss
+        ep_loss = 0
         for i in range(batch_size):
             #feedforward to generate action
             observation = np.array([target_pos[0], target_pos[1], data.qpos[0], data.qvel[0], data.qpos[1], data.qvel[1]])
-            observation_tensor = torch.from_numpy(observation).float()
-            u_tensor = clamped_net(observation_tensor)
-            u = u_tensor.numpy()
-            data.ctrl[0:4] = u[0:4]
+            observation_tensor = torch.tensor(observation, requires_grad=True, dtype=torch.float32)
+            u_tensor = net(observation_tensor.view(1, 6)) #1x4
+            u = u_tensor.detach().numpy()
+            data.ctrl[0:4] = u[0]
 
             #simulation with action to genearsate new state
             mj.mj_step(model, data)
             B = np.zeros((2*model.nv + model.na, model.nu), dtype=np.float64)
-            mj.mjd_transitionFD(m=model, d=data, eps=0.0001, flag_centered=0, B=B)
-            new_state_tensor = torch.from_numpy(np.array(data.qpos[0], data.qpos[1])).float()
-            target_state_tensor = torch.from_numpy(target_pos).float()
+            mj.mjd_transitionFD(model, data, 0.0001, 0, None, B, None, None)
+            new_state_tensor = torch.tensor(np.array([data.qpos[0], data.qpos[1]]), requires_grad=True, dtype=torch.float32)
+            target_state_tensor = torch.tensor(target_pos, requires_grad=False)
 
             #calculate loss
             loss = torch.norm(target_state_tensor - new_state_tensor, p=2)
+            ep_loss += loss
             loss.backward()
 
             #compute gradient of loss wrt u
             grad_physics = np.array([[B[0][0], B[0][1], B[0][2], B[0][3]],
                                     [B[1][0], B[1][1], B[1][2], B[1][3]]])
-            grad_physics_tensor = torch.from_numpy(grad_physics).float()
-            grad_loss_wrt_u = torch.matmul(grad_physics_tensor, new_state_tensor.grad)
+            grad_physics_tensor = torch.tensor(grad_physics, requires_grad=False, dtype=torch.float32) #2x4
+            grad_loss_wrt_u_tensor = torch.matmul(new_state_tensor.grad.view(1, 2), grad_physics_tensor) #1x4
 
             #compute overall graident
-            I = np.identity(4)
-            for act_i in range(4):
-                dummy_loss = I[act_i][0] * u_tensor[0] + I[act_i][1] * u_tensor[1] + I[act_i][2] * u_tensor[2] + I[act_i][3] * u_tensor[3]
-                dummy_loss.backward()
-                for param in net.parameters():
-                    if param.requires_grad:
-                        param.data.grad = param.data.grad * grad_loss_wrt_u[act_i] #this part is currently wrong
+            u_tensor.backward(grad_loss_wrt_u_tensor)
+
+        mean_ep_loss += ep_loss
+        #update network
+        optimizer.step()
+
+    mean_ep_loss /= num_of_targets
+    print(f"epoch: {epoch}, mean_ep_loss: {mean_ep_loss}")
+    # print("grad")
+    # for param in net.parameters():
+    #     if param.requires_grad:
+    #         print(param.data.grad)
 
 
 
