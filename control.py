fixed rotation direction issue for different cocontraction

control.py
@@ -589,13 +589,14 @@ class TemplateController(object):
         self.action = np.zeros(2)
         self.target_pos = np.zeros(2)
         self.env_id = env_id
+        self.cocontraction = 0
 
     def callback(self, model, data):
         for i in range(1):
             if self.target_pos[i] >= 0:
-                data.ctrl[i * 2] = self.action[i]
+                data.ctrl[i * 2] = self.action[i] + self.cocontraction
             else:
-                data.ctrl[i * 2 + 1] = self.action[i]
+                data.ctrl[i * 2 + 1] = self.action[i] + self.cocontraction
 
     def set_action(self, newaction):
         for i in range(1):

main1.py
@@ -6,10 +6,10 @@ from mujoco.glfw import glfw
 import pickle
 from control import *
 
-m_target = np.array([0.9, -0.45])
+m_target = np.array([0.43, -0.45])
 # m_target = np.array([-10, 0])
-cocontraction = 0.5
-modelid = "1702516376"
+cocontraction = 0.7
+modelid = "1702619395"
 #######################################################################
 # Load Params
 print("\n\n")
@@ -153,6 +153,7 @@ def callback(model, data):
         for i in range(controller_params.output_size):
             u[i] = u_tensor[0][i].item()
         controller.set_action(u)
+        controller.cocontraction = cocontraction
         for i in range(1):
             if controller.target_pos[i] >= 0:
                 data.ctrl[i * 2 + 1] = cocontraction
@@ -161,7 +162,7 @@ def callback(model, data):
         global_timer = data.time
 
     controller.callback(model, data)
-    print(data.qpos)
+    print(f"{data.qpos}-{data.ctrl}")
     # print(data.ctrl)
 
 

main2.py
@@ -21,7 +21,8 @@ feedforward_model_path0 = "./1702020394.pth" #wide range model
 ff_net = torch_net.FeedForwardNN(2, 32, 4, Control_Type.BASELINE)
 ff_net.load_state_dict(torch.load(feedforward_model_path0))
 ff_net.eval()
-xml_path = 'double_links_fast.xml'
+# xml_path = 'double_links_fast.xml'
+xml_path = 'single_link.xml'
 
 sim_pause = True
 next_frame = False
@@ -36,7 +37,7 @@ lastx = 0
 lasty = 0
 
 # Neuron Controller
-controller = BaselineController(controller_params, env_id)
+# controller = BaselineController(controller_params, env_id)
 
 def keyboard(window, key, scancode, act, mods):
     if act == glfw.PRESS and key == glfw.KEY_BACKSPACE:
@@ -120,23 +121,25 @@ def scroll(window, xoffset, yoffset):
 def init_controller(model,data):
     mj.mj_resetData(model, data)
     mj.mj_forward(model, data)
-    controller.target_pos = np.array([m_target[0], m_target[1]])
+    # controller.target_pos = np.array([m_target[0], m_target[1]])
 
 ep_error = 0
 def callback(model, data):
     global global_timer, ep_error
     if data.time - global_timer >= dt_brain or data.time < 0.000101:
-        observation_tensor = torch.tensor(controller.target_pos, requires_grad=False, dtype=torch.float32)
-        u_tensor = ff_net(observation_tensor.view(1, 2))
-        u = np.zeros(4)
-        for i in range(4):
-            u[i] = u_tensor[0][i].item()
-        controller.set_action(u)
+        # observation_tensor = torch.tensor(controller.target_pos, requires_grad=False, dtype=torch.float32)
+        # u_tensor = ff_net(observation_tensor.view(1, 2))
+        # u = np.zeros(4)
+        # for i in range(4):
+        #     u[i] = u_tensor[0][i].item()
+        # controller.set_action(u)
+        data.ctrl[0] = 0.23001
+        data.ctrl[1] = 0.2
         global_timer = data.time
 
-    controller.callback(model, data)
-    # print(data.qpos)
-    print(data.ctrl)
+    # controller.callback(model, data)
+    print(data.qpos)
+    # print(data.ctrl)
 
 
 #get the full path

physics_grad.py
@@ -103,7 +103,7 @@ class physics(torch.autograd.Function):
         data_after_simulation = ctx.data_after_simulation
         steps_simulated = ctx.steps_simulated
 
-        eps = 0.000001
+        eps = 0.00001
         grad = np.zeros((joint_number, u_dim))
 
         u = np.zeros(u_dim)

train_spinal.py
@@ -35,7 +35,7 @@ net = torch_net.FeedForwardNN(input_size, hidden_size, output_size, pa.control_t
 
 # traning configuration
 num_epochs = 5000
-learning_rate = 0.0005
+learning_rate = 0.0003
 
 # intialize simutlation parameters
 dt_brain = pa.controller_params.brain_dt
@@ -54,6 +54,7 @@ for epoch in range(num_epochs):
         # cocontraction = traning_samples[batch_id][2]
         pa.controller.target_pos[0] = traning_samples[batch_id][0]
         cocontraction = traning_samples[batch_id][1]
+        pa.controller.cocontraction = cocontraction
         mj.mj_resetData(pa.model, pa.data)
         mj.mj_forward(pa.model, pa.data)
 
@@ -98,7 +99,7 @@ for epoch in range(num_epochs):
     writer.add_scalar("Loss/mean_ep_loss", mean_ep_loss, epoch)
     if epoch % 50 == 0:
         torch.save(net.state_dict(), f'{models_dir}/{int(time.time())}.pth')
-    if mean_ep_loss < 0.02:
+    if mean_ep_loss < 0.01:
         break
 
 writer.flush()