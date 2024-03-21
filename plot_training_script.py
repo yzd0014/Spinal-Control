5e0db109fafdda88130266c9cd7cd9ccf573a402
Yitong Dai <1051714925@qq.com>
2024-03-20 23:28:18 -0700

    boxplot fix

--- plot_training_script.py
+++ plot_training_script.py
@@ -4,9 +4,11 @@ import seaborn as sns
 
 datafile = './datalog/baesline-processed.pkl'
 data = pd.read_pickle(datafile)
-data = data[data['Model'] == 'baseline']
-value = data.Value.to_numpy()
-all_data = [value, value]
+data_baseline = data[data['Model'] == 'baseline']
+data_sac = data[data['Model'] == 'sac']
+value_baseline = data_baseline.Value.to_numpy()
+value_sac = data_sac.Value.to_numpy()
+all_data = [value_baseline, value_sac]
 labels = ['baseline', 'sac']
 plt.boxplot(all_data, labels=labels)
 plt.show()
